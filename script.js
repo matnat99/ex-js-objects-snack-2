d849a81620a9f7c6fe245600521065616b82f12e
Mattia Natale <mattianatale07@gmail.com>
2025-04-08 18:51:05 +0200

    feat: add code question 5

--- script.js
+++ script.js
@@ -99,5 +99,44 @@ Qual è il metodo migliore per clonare l’oggetto chef, e perché?
 Il metodo migliore per clonare l'oggetto chef è la shallow copy perché può copiare anche funzioni interne a un oggetto
 
 Qual è il metodo migliore per clonare l’oggetto restaurant, e perché?
-Per clonare l'oggetto restaurant il metodo migliore è structuredClone perché può copiare anche oggetti complessi annidati nell'oggetto da copiare
+Per clonare l'oggetto restaurant il metodo migliore è structuredClone perché fa una copia profonda 
+e può copiare anche oggetti complessi annidati nell'oggetto da clonare
+*/
+
+//  Code Question 5 (Bonus)
+
+{
+  const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    maker: {
+      name: "Anonymous Chef",
+      restaurant: {
+        name: "Hyur's Burgers",
+        address: "Main Street, 123",
+        isOpen: true,
+      },
+      age: 29,
+    },
+  };
+
+  const newRestaurant = { ...hamburger.maker.restaurant };
+  newRestaurant.name = "Hyur's II";
+  newRestaurant.address = "Second Street, 12";
+  const secondBurger = { ...hamburger };
+  secondBurger.maker.restaurant = newRestaurant;
+  secondBurger.maker.name = "Chef Hyur";
+
+  console.log(hamburger.maker.name); // ?
+  console.log(secondBurger.maker.name); // ?
+  console.log(hamburger.maker.restaurant.name); // ?
+  console.log(secondBurger.maker.restaurant.name); // ?
+}
+
+/*
+Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+In console verranno stampati per due volte "Chef Hyur" e "Hyur's II"
+
+Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+5
 */
