b90a479c6b33bf9237842732d5d11949c9890ef6
Mattia Natale <mattianatale07@gmail.com>
2025-04-08 18:10:53 +0200

    feat: add code question 2

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ e quando vengono modificate le proprietà di secondBurger si modificano anche qu
 Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 
 1
 */
+
+// Code Question 2
+
+{
+  const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    ingredients: ["Cheese", "Meat", "Bread", "Tomato"],
+  };
+
+  const secondBurger = { ...hamburger };
+  secondBurger.ingredients[0] = "Salad";
+
+  console.log(hamburger.ingredients[0]); // ?
+  console.log(secondBurger.ingredients[0]); // ?
+}
+
+/*
+  P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi di Riferimento)!
+  Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+  In console verrà stampato per due volte "Salad" perché secondBurger è un nuovo oggetto ma una shallow copy dell'oggetto hamburger e quando
+  viene modificato l'elemento con indice 0 dell' array ingredients in secondBurger si modifica anche in hamburger perché ingredients è un altro
+  livello di annidamento
+  
+  Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+  3
+  */
