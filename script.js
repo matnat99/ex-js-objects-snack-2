7a2490513ef7ee4d3d4f3a5ef96d9adfad733b0a
Mattia Natale <mattianatale07@gmail.com>
2025-04-08 18:18:05 +0200

    feat: add code question 3

--- script.js
+++ script.js
@@ -44,4 +44,30 @@ Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codic
   
   Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
   3
-  */
+*/
+
+// Code Question 3
+
+{
+  const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    maker: {
+      name: "Anonymous Chef",
+      restaurant: {
+        name: "Hyur's Burgers",
+        address: "Main Street, 123",
+        isOpen: true,
+      },
+      age: 29,
+    },
+  };
+
+  const secondBurger = structuredClone(hamburger);
+  const thirdBurger = structuredClone(hamburger);
+}
+
+/*
+Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+9
+*/
